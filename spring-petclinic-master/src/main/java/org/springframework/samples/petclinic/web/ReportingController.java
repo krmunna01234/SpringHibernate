b826f306ab2faa1fc9c0eca7cbe89bc62a594003
krmunna01234 <krmunna01234@gmail.com>
2016-08-15 22:27:53 +0530

    commit test

--- spring-petclinic-master/src/main/java/org/springframework/samples/petclinic/web/ReportingController.java
+++ spring-petclinic-master/src/main/java/org/springframework/samples/petclinic/web/ReportingController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
+//// commit test !!
+
 @Controller
 public class ReportingController {
 	private final ClinicService clinicService;
